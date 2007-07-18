3c232588be4ec24cdb19b1a4110b21456a690d6f
yanick <yanick>
2007-07-18 02:47:17 +0000

    r1086@enkidu:  yanick | 2007-07-17 22:20:24 -0400  change version to 0.06

--- lib/Pod/Manual.pm
+++ lib/Pod/Manual.pm
@@ -15,7 +15,7 @@ use XML::XPathScript;
 use Pod::Manual::PodXML2Docbook;
 use Pod::Manual::Docbook2LaTeX;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 my @parser_of        :Field;
 my @dom_of           :Field;
@@ -243,7 +243,7 @@ Pod::Manual - Aggregates several PODs into a single manual
 
 =head1 VERSION
 
-This document describes Pod::Manual version 0.05
+This document describes Pod::Manual version 0.06
 
 As you can guess from the very low version number, this release
 is alpha quality. Use with caution.
