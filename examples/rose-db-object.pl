61496fb479cc3ba2945bb0f70daa2ac2dc706088
Yanick Champoux <yanick@babyl.dyndns.org>
2007-11-17 20:25:25 -0500

    beefing up the document

--- examples/rose-db-object.pl
+++ examples/rose-db-object.pl
@@ -7,7 +7,13 @@ use Pod::Manual;
 
 my $manual = Pod::Manual->new({ title => 'Rose::DB::Object' });
 
-$manual->add_chapters( qw/ Rose::DB::Object::Tutorial / );
+$manual->add_chapters( qw/ Rose::DB::Object::Tutorial 
+                           Rose::DB::Tutorial
+                           Rose::DB::Object
+                           Rose::DB::Object::Metadata
+                           Rose::DB::Object::Manager
+                           Rose::DB::SQLite
+                         / );
 
 my $pdf_file = 'rose-db-object_manual.pdf';
 $manual->save_as_pdf( $pdf_file );
